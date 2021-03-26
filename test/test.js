feb7dfb91a3c255764a31730332a2ab474680c88
Antò Mela <heavnzor@protonmail.com>
2021-03-26 22:54:12 +0100

    ok

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
-const numbers = [1, 3, 4, 5, 3, 4]
-
+console.log ('anto');
+numbers = [1, 2, 3, 4, 3, 2, 1, 34, 4, 'text'];
 for (const number of numbers) {
-  console.log(number)
+  console.log (number);
+  if(typeof number == String){
+    number = number.length
+  }
 }
 
