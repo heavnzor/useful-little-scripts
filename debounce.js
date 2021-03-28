a97d90366089e03cedb9a64684278442da137982
Antò Mela <heavnzor@protonmail.com>
2021-03-28 10:44:41 +0200

    debounce

---
+++ debounce.js
@@ -0,0 +1,23 @@
+let debounce;
+
+debounce = (callback, delay) => {
+
+    var timer;
+
+    return function(){
+
+        var args = arguments;
+
+        var context = this;
+
+        clearTimeout(timer);
+
+        timer = setTimeout(function(){
+
+            callback.apply(context, args);
+
+        }, delay)
+
+    }
+
+};
---
+++ test/prog.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const {say} = require('cowsay');
+
+const message = say({ text: 'Mon premier import de package !' });
+
+console.log(message);
